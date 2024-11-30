3815e1a440d735377046c23c6fc580f6c8348e5a
Balaji B <balaji.balachandar1@gmail.com>
2024-11-30 15:49:15 +0530

    Final Commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import smtplib
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
@@ -8,6 +9,13 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditor, CKEditorField
 from datetime import datetime
+from dotenv import load_dotenv
+import os
+load_dotenv("C:/Python/Environmental variables/.env")
+
+my_mail = "sampleforpythonmail@gmail.com"
+password = os.getenv("smtp_app_password")
+
 
 '''
 Make sure the required packages are installed: 
@@ -73,11 +81,10 @@ def show_post(post_id):
     return render_template("post.html", post=requested_post)
 
 
-# TODO: add_new_post() to create a new blog post
+
 @app.route("/new-post",methods=["POST","GET"])
 def add_new_post():
     form = NewPostForm()
-    edit_post = request.args.get("query_post")
     if form.validate_on_submit():
         new_post = BlogPost(
             title = form.title.data,
@@ -93,23 +100,61 @@ def add_new_post():
     return render_template("make-post.html",form=form)
 
 
-# TODO: edit_post() to change an existing blog post
+
 @app.route("/edit-post/<post_id>",methods = ["POST","GET"])
 def edit_post(post_id):
     query_post = db.session.execute(db.select(BlogPost).where(BlogPost.id==post_id)).scalars().all()[0]
-    return redirect(url_for("add_new_post",query_post = query_post))
+    edit_form = NewPostForm(
+            title=query_post.title,
+            sub_title=query_post.subtitle,
+            author=query_post.author,
+            body=query_post.body,
+            img_url=query_post.img_url,
+        )
+    if edit_form.validate_on_submit():
+        query_post.title = edit_form.title.data
+        query_post.subtitle = edit_form.sub_title.data
+        query_post.author =edit_form.author.data
+        query_post.body =edit_form.body.data
+        query_post.img_url = edit_form.img_url.data
+        db.session.commit()
+        return redirect(url_for("show_post", post_id=query_post.id))
+    return render_template("make-post.html",edit_form=edit_form)
 
 
-# TODO: delete_post() to remove a blog post from the database
+
+@app.route("/delete/<post_id>")
+def delete_post(post_id):
+    ## another way of selecting a specific row(post) --------------
+    query_post =db.get_or_404(BlogPost, post_id)
+    ##--------------------------------------------------------------
+    db.session.delete(query_post)
+    db.session.commit()
+    return redirect(url_for("get_all_posts"))
 
 @app.route("/about")
 def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
+@app.route('/contact',methods=["POST","GET"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "GET":
+        return render_template("contact.html")
+    if request.method == "POST":
+        data = request.form
+        with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+            connection.starttls()
+            connection.login(my_mail, password=password)
+            connection.sendmail(from_addr=my_mail,
+                                to_addrs=my_mail,
+                                msg=f"Subject:Message from Blog\n\n"
+                                    f"Name : {data['name']}\n"
+                                    f"Email : {data['email']}\n"
+                                    f"Phone : {data['phone']}\n"
+                                    f"Message : {data['message']}")
+        return render_template("contact.html",submitted = True)
+
 
 
 if __name__ == "__main__":
